8555c232af7ba081287264eccca5d2fb153b1271
Kurt Choi <choi.kurt3@gmail.com>
2021-03-03 19:55:50 -0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import WorkoutsList from './components/WorkoutsList';
+import EditWorkout from './components/EditWorkout';
+import NewWorkout from './components/NewWorkout';
+import NewUser from './components/NewUser';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path='/' exact component={WorkoutsList} />
+        <Route path='/edit/:id' component={EditWorkout} />
+        <Route path='/new' component={NewWorkout} />
+        <Route path='/user' component={NewUser} />
+      </div>
+    </Router>
   );
 }
 
